388ae7435d884250b89285e5dcb995861747f7ee
Lucas Wakefield <lucas.wakefield@gmail.com>
2022-05-01 15:50:27 -0500

    updated formatting to follow linter rules

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,18 +1,18 @@
+/* eslint-disable react/no-array-index-key */
 import React from 'react';
 
 const Display = ({ numsArr }) => {
-	const even = { color: 'red' };
-	const odd = { color: 'blue'};
+  const even = { color: 'red' };
+  const odd = { color: 'blue' };
 
-	return (
-		numsArr.map((el, idx) => {
-			if(el % 2 === 0) {
-				return (<div key={idx} style={even}>{el}</div>)
-			} else {
-				return (<div key={idx} style={odd}>{el}</div>)
-			};
-		})
-	)
-}
+  return (
+    numsArr.map((el, idx) => {
+      if (el % 2 === 0) {
+        return (<div key={idx} style={even}>{el}</div>);
+      }
+      return (<div key={idx} style={odd}>{el}</div>);
+    })
+  );
+};
 
 export default Display;
